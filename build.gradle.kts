99864f822d2a40e0c14a0b6669ac86dfd0566f30
lunarydess <lucielle.hoerner@outlook.com>
2024-09-09 22:23:52 +0200

    :sparkles: pushed changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,46 +1,45 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import java.nio.charset.StandardCharsets
 import java.net.URI
-import java.util.Properties
+import java.nio.charset.StandardCharsets
+import java.util.*
 
 plugins {
-    id("idea")
+  id("idea")
 
-    id("java")
-    id("java-library")
-
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+  id("java")
+  id("java-library")
 
   id("maven-publish")
+  id("com.gradleup.shadow") version "8.3.0"
 }
 
-group = "net.cakemc"
+group = "net.cakemc.mc"
 version = "0.0.0-develop"
 
 val repoProperties = Properties()
-val credFile = file("credentials.properties")
-if (credFile.exists()) {
-    repoProperties.load(file("credentials.properties").inputStream())   
-}
-val repoUsername = repoProperties.getProperty("username", System.getenv("REPOSITORY_USERNAME"))
-val repoPassword = repoProperties.getProperty("password", System.getenv("REPOSITORY_PASSWORD"))
+val repoFile = file("credentials.properties")
+if (repoFile.exists())
+  repoProperties.load(repoFile.inputStream())
+val repoUsername: String = (repoProperties["username"] ?: System.getenv("REPOSITORY_USERNAME")).toString()
+val repoPassword: String = (repoProperties["password"] ?: System.getenv("REPOSITORY_PASSWORD")).toString()
 
 repositories {
-    mavenCentral()
-    mavenLocal()
-    maven {
-      name = "cakemc"
-      url = URI.create("http://cakemc.net:8081/repository/maven-releases")
-      credentials {
-        username = repoUsername
-        password = repoPassword
-      }
-      isAllowInsecureProtocol = true
+  mavenCentral()
+  mavenLocal()
+  maven {
+    name = "cakemc"
+    url = URI.create("http://cakemc.net:8081/repository/maven-releases")
+    credentials {
+      username = repoUsername
+      password = repoPassword
     }
+    isAllowInsecureProtocol = true
+  }
 }
 
+@Suppress("unchecked_cast")
 fun <V> prop(value: String): V {
-    return properties.getValue(value) as V
+  return properties.getValue(value) as V
 }
 
 publishing {
@@ -61,261 +60,332 @@ publishing {
 }
 
 dependencies {
-    implementation("net.cakemc:library:0.0.0-develop:all")
-    implementation("net.cakemc:protocol:0.0.0-develop:all")
+  implementation(
+    group = "net.cakemc.mc",
+    name = "lib",
+    version = "0.0.0-develop"
+  )
+  shadow(
+    group = "net.cakemc.mc",
+    name = "lib",
+    version = "0.0.0-develop"
+  )
+  implementation(
+    group = "net.cakemc.mc",
+    name = "protocol",
+    version = "0.0.0-develop"
+  )
+  shadow(
+    group = "net.cakemc.mc",
+    name = "protocol",
+    version = "0.0.0-develop"
+  )
 
-    annotationProcessor(
-        group = "org.jetbrains",
-        name = "annotations",
-        version = prop("dep_ann-jbr")
-    )
-    implementation(
-        group = "org.jetbrains",
-        name = "annotations",
-        version = prop("dep_ann-jbr")
-    )
+  annotationProcessor(
+    group = "org.jetbrains",
+    name = "annotations",
+    version = prop("dep_ann-jbr")
+  )
+  implementation(
+    group = "org.jetbrains",
+    name = "annotations",
+    version = prop("dep_ann-jbr")
+  )
 
+  annotationProcessor(
+    group = "com.fasterxml.jackson.core",
+    name = "jackson-annotations",
+    version = prop("dep-jackson")
+  );
+  implementation(
+    group = "com.fasterxml.jackson.core",
+    name = "jackson-annotations",
+    version = prop("dep-jackson")
+  );
+  shadow(
+    group = "com.fasterxml.jackson.core",
+    name = "jackson-annotations",
+    version = prop("dep-jackson")
+  );
 
-    annotationProcessor(
-        group = "com.fasterxml.jackson.core",
-        name = "jackson-annotations",
-        version = prop("dep-jackson")
-    );
-    implementation(
-        group = "com.fasterxml.jackson.core",
-        name = "jackson-annotations",
-        version = prop("dep-jackson")
-    );
-    shadow(
-        group = "com.fasterxml.jackson.core",
-        name = "jackson-annotations",
-        version = prop("dep-jackson")
-    );
+  implementation(
+    group = "com.fasterxml.jackson.core",
+    name = "jackson-core",
+    version = prop("dep-jackson")
+  );
+  shadow(
+    group = "com.fasterxml.jackson.core",
+    name = "jackson-core",
+    version = prop("dep-jackson")
+  );
 
-    implementation(
-        group = "com.fasterxml.jackson.core",
-        name = "jackson-core",
-        version = prop("dep-jackson")
-    );
-    shadow(
-        group = "com.fasterxml.jackson.core",
-        name = "jackson-core",
-        version = prop("dep-jackson")
-    );
+  implementation(
+    group = "com.fasterxml.jackson.core",
+    name = "jackson-databind",
+    version = prop("dep-jackson")
+  );
+  shadow(
+    group = "com.fasterxml.jackson.core",
+    name = "jackson-databind",
+    version = prop("dep-jackson")
+  );
 
-    implementation(
-        group = "com.fasterxml.jackson.core",
-        name = "jackson-databind",
-        version = prop("dep-jackson")
-    );
-    shadow(
-        group = "com.fasterxml.jackson.core",
-        name = "jackson-databind",
-        version = prop("dep-jackson")
-    );
+  implementation(
+    group = "io.github.speiger",
+    name = "Primitive-Collections",
+    version = prop("dep-reinspeigern")
+  )
+  shadow(
+    group = "io.github.speiger",
+    name = "Primitive-Collections",
+    version = prop("dep-reinspeigern")
+  )
 
-    implementation(
-        group = "io.github.speiger",
-        name = "Primitive-Collections",
-        version = prop("dep-reinspeigern")
-    )
-    shadow(
-        group = "io.github.speiger",
-        name = "Primitive-Collections",
-        version = prop("dep-reinspeigern")
-    )
+  implementation(
+    group = "io.netty",
+    name = "netty-common",
+    version = prop("dep-netty")
+  )
+  shadow(
+    group = "io.netty",
+    name = "netty-common",
+    version = prop("dep-netty")
+  )
 
-    implementation(
-        group = "io.netty",
-        name = "netty-common",
-        version = prop("dep-netty")
-    )
-    shadow(
-        group = "io.netty",
-        name = "netty-common",
-        version = prop("dep-netty")
-    )
+  implementation(
+    group = "io.netty",
+    name = "netty-buffer",
+    version = prop("dep-netty")
+  )
+  shadow(
+    group = "io.netty",
+    name = "netty-buffer",
+    version = prop("dep-netty")
+  )
 
-    implementation(
-        group = "io.netty",
-        name = "netty-buffer",
-        version = prop("dep-netty")
-    )
-    shadow(
-        group = "io.netty",
-        name = "netty-buffer",
-        version = prop("dep-netty")
-    )
+  implementation(
+    group = "io.netty",
+    name = "netty-codec",
+    version = prop("dep-netty")
+  )
+  shadow(
+    group = "io.netty",
+    name = "netty-codec",
+    version = prop("dep-netty")
+  )
 
-    implementation(
-        group = "io.netty",
-        name = "netty-codec",
-        version = prop("dep-netty")
-    )
-    shadow(
-        group = "io.netty",
-        name = "netty-codec",
-        version = prop("dep-netty")
-    )
+  implementation(
+    group = "io.netty",
+    name = "netty-resolver",
+    version = prop("dep-netty")
+  )
+  shadow(
+    group = "io.netty",
+    name = "netty-resolver",
+    version = prop("dep-netty")
+  )
 
-    implementation(
-        group = "io.netty",
-        name = "netty-resolver",
-        version = prop("dep-netty")
-    )
-    shadow(
-        group = "io.netty",
-        name = "netty-resolver",
-        version = prop("dep-netty")
-    )
+  implementation(
+    group = "io.netty",
+    name = "netty-codec",
+    version = prop("dep-netty")
+  )
+  shadow(
+    group = "io.netty",
+    name = "netty-codec",
+    version = prop("dep-netty")
+  )
 
-    implementation(
-        group = "io.netty",
-        name = "netty-codec",
-        version = prop("dep-netty")
-    )
-    shadow(
-        group = "io.netty",
-        name = "netty-codec",
-        version = prop("dep-netty")
-    )
+  implementation(
+    group = "io.netty",
+    name = "netty-transport",
+    version = prop("dep-netty")
+  )
+  shadow(
+    group = "io.netty",
+    name = "netty-transport",
+    version = prop("dep-netty")
+  )
 
-    implementation(
-        group = "io.netty",
-        name = "netty-transport",
-        version = prop("dep-netty")
-    )
-    shadow(
-        group = "io.netty",
-        name = "netty-transport",
-        version = prop("dep-netty")
-    )
+  implementation(
+    group = "io.netty",
+    name = "netty-transport-classes-epoll",
+    version = prop("dep-netty")
+  )
+  shadow(
+    group = "io.netty",
+    name = "netty-transport-classes-epoll",
+    version = prop("dep-netty")
+  )
 
-    implementation(
-        group = "io.netty",
-        name = "netty-transport-classes-epoll",
-        version = prop("dep-netty")
-    )
-    shadow(
-        group = "io.netty",
-        name = "netty-transport-classes-epoll",
-        version = prop("dep-netty")
-    )
+  implementation(
+    group = "io.netty",
+    name = "netty-transport-classes-kqueue",
+    version = prop("dep-netty")
+  )
+  shadow(
+    group = "io.netty",
+    name = "netty-transport-classes-kqueue",
+    version = prop("dep-netty")
+  )
 
-    implementation(
-        group = "io.netty",
-        name = "netty-transport-classes-kqueue",
-        version = prop("dep-netty")
-    )
-    shadow(
-        group = "io.netty",
-        name = "netty-transport-classes-kqueue",
-        version = prop("dep-netty")
-    )
+  implementation(
+    group = "io.netty",
+    name = "netty-transport-native-epoll",
+    version = prop("dep-netty"),
+    classifier = "linux-aarch_64"
+  )
+  shadow(
+    group = "io.netty",
+    name = "netty-transport-native-epoll",
+    version = prop("dep-netty"),
+    classifier = "linux-aarch_64"
+  )
 
-    implementation(
-        group = "io.netty",
-        name = "netty-transport-native-epoll",
-        version = prop("dep-netty"),
-        classifier = "linux-aarch_64"
-    )
-    shadow(
-        group = "io.netty",
-        name = "netty-transport-native-epoll",
-        version = prop("dep-netty"),
-        classifier = "linux-aarch_64"
-    )
+  implementation(
+    group = "io.netty",
+    name = "netty-transport-native-epoll",
+    version = prop("dep-netty"),
+    classifier = "linux-riscv64"
+  )
+  shadow(
+    group = "io.netty",
+    name = "netty-transport-native-epoll",
+    version = prop("dep-netty"),
+    classifier = "linux-riscv64"
+  )
 
-    implementation(
-        group = "io.netty",
-        name = "netty-transport-native-epoll",
-        version = prop("dep-netty"),
-        classifier = "linux-riscv64"
-    )
-    shadow(
-        group = "io.netty",
-        name = "netty-transport-native-epoll",
-        version = prop("dep-netty"),
-        classifier = "linux-riscv64"
-    )
+  implementation(
+    group = "io.netty",
+    name = "netty-transport-native-epoll",
+    version = prop("dep-netty"),
+    classifier = "linux-x86_64"
+  )
+  shadow(
+    group = "io.netty",
+    name = "netty-transport-native-epoll",
+    version = prop("dep-netty"),
+    classifier = "linux-x86_64"
+  )
 
-    implementation(
-        group = "io.netty",
-        name = "netty-transport-native-epoll",
-        version = prop("dep-netty"),
-        classifier = "linux-x86_64"
-    )
-    shadow(
-        group = "io.netty",
-        name = "netty-transport-native-epoll",
-        version = prop("dep-netty"),
-        classifier = "linux-x86_64"
-    )
+  implementation(
+    group = "io.netty",
+    name = "netty-transport-native-kqueue",
+    version = prop("dep-netty"),
+    classifier = "osx-aarch_64"
+  )
+  shadow(
+    group = "io.netty",
+    name = "netty-transport-native-kqueue",
+    version = prop("dep-netty"),
+    classifier = "osx-aarch_64"
+  )
 
-    implementation(
-        group = "io.netty",
-        name = "netty-transport-native-kqueue",
-        version = prop("dep-netty"),
-        classifier = "osx-aarch_64"
-    )
-    shadow(
-        group = "io.netty",
-        name = "netty-transport-native-kqueue",
-        version = prop("dep-netty"),
-        classifier = "osx-aarch_64"
-    )
+  implementation(
+    group = "io.netty",
+    name = "netty-transport-native-kqueue",
+    version = prop("dep-netty"),
+    classifier = "osx-x86_64"
+  )
+  shadow(
+    group = "io.netty",
+    name = "netty-transport-native-kqueue",
+    version = prop("dep-netty"),
+    classifier = "osx-x86_64"
+  )
 
-    implementation(
-        group = "io.netty",
-        name = "netty-transport-native-kqueue",
-        version = prop("dep-netty"),
-        classifier = "osx-x86_64"
-    )
-    shadow(
-        group = "io.netty",
-        name = "netty-transport-native-kqueue",
-        version = prop("dep-netty"),
-        classifier = "osx-x86_64"
-    )
+  implementation(
+    group = "io.netty",
+    name = "netty-handler",
+    version = prop("dep-netty")
+  )
+  shadow(
+    group = "io.netty",
+    name = "netty-handler",
+    version = prop("dep-netty")
+  )
 
-    implementation(
-        group = "io.netty",
-        name = "netty-handler",
-        version = prop("dep-netty")
-    )
-    shadow(
-        group = "io.netty",
-        name = "netty-handler",
-        version = prop("dep-netty")
-    )
+  implementation(
+    group = "io.netty",
+    name = "netty-handler-proxy",
+    version = prop("dep-netty")
+  )
+  shadow(
+    group = "io.netty",
+    name = "netty-handler-proxy",
+    version = prop("dep-netty")
+  )
+}
 
-    implementation(
-        group = "io.netty",
-        name = "netty-handler-proxy",
-        version = prop("dep-netty")
-    )
-    shadow(
-        group = "io.netty",
-        name = "netty-handler-proxy",
-        version = prop("dep-netty")
-    )
+val jdkVersion = JavaVersion.VERSION_21
+val jdkVersionString = jdkVersion.toString()
 
+java {
+  toolchain.languageVersion = JavaLanguageVersion.of(jdkVersionString)
+  withSourcesJar()
 }
 
 tasks.withType<JavaCompile> {
-    sourceCompatibility = JavaVersion.VERSION_21.toString()
-    targetCompatibility = JavaVersion.VERSION_21.toString()
-    options.encoding = StandardCharsets.UTF_8.toString()
+  sourceCompatibility = jdkVersionString
+  targetCompatibility = jdkVersionString
+  options.encoding = StandardCharsets.UTF_8.toString()
 }
 
 tasks.withType<AbstractArchiveTask> {
-    isReproducibleFileOrder = true
-    isPreserveFileTimestamps = false
+  isReproducibleFileOrder = true
+  isPreserveFileTimestamps = false
 }
 
 tasks.withType<ShadowJar> {
-    configurations = listOf(project.configurations.shadow.get())
-    isZip64 = true
+  configurations = listOf(project.configurations.shadow.get())
+  isZip64 = true
 }
 
 configurations.shadow { isTransitive = false }
+
+publishing {
+  publications {
+    create<MavenPublication>("mavenCakeNexus") {
+      from(components["java"])
+      artifact(tasks.shadowJar)
+
+      groupId = group.toString()
+      artifactId = "proxy"
+      version = version
+
+      pom {
+        name.set("CakeMc minecraft proxy")
+        description.set("The proxy of our server.")
+        url.set("https://github.com/CakeMC-Network/minecraft-proxy")
+        licenses {
+          license {
+            name.set("Apache-2.0")
+            url.set("https://www.apache.org/licenses/LICENSE-2.0")
+          }
+        }
+        developers {
+          developer {
+            id.set("CakeMcNET")
+            name.set("CakeMc")
+          }
+        }
+        scm {
+          connection = "scm:git:git://github.com/CakeMC-Network/minecraft-proxy.git"
+          developerConnection = "scm:git:ssh://github.com/CakeMC-Network/minecraft-proxy.git"
+          url = "github.com/CakeMC-Network/minecraft-proxy"
+        }
+      }
+    }
+  }
+  repositories {
+    maven {
+      name = "cakemc-nexus"
+      url = uri("http://cakemc.net:8081/")
+      credentials {
+        username = repoUsername
+        password = repoPassword
+      }
+      isAllowInsecureProtocol = true
+    }
+  }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,6 @@
 rootProject.name = "minecraft-proxy"
+
+plugins {
+  id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
